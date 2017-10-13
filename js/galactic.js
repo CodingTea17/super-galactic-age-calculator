63d865cafc6495a987e8ad1fee1490e88b82d1d5
Dawson Mortenson <dawson.mortenson@gmail.com>
2017-10-13 10:01:54 -0700

    add a function to make secsBetween pass

--- js/galactic.js
+++ js/galactic.js
@@ -8,4 +8,11 @@ export class Galactic {
     // Multiplies and age by days in an earth year, seonds in an earth day, and 1000 (to achieve milliseconds). It floors the result for consistent rounding
     return Math.floor(age * this.earth_days_in_year * this.earth_seconds_per_day * 1000);
   }
+
+  secsBetween(startDate,endDate){
+    startDate = new Date(startDate.year,startDate.month,startDate.day,0,0,0,0);
+    endDate = new Date(endDate.year,endDate.month,endDate.day,0,0,0,0);
+    return Math.abs((startDate.getTime() - endDate.getTime()) / 1000);
+  }
+
 }
--- spec/galactic-spec.js
+++ spec/galactic-spec.js
@@ -13,7 +13,7 @@ describe('Galactic', () => {
   it('accepts to dates to compare the time (in seconds) between them', () => {
     let epicodusStart = {month:7, day:31, year:2017};
     let epicodusGraduation = {month:12, day:21, year:2017};
-    // July: 1 day, Aug: 31 days, Sept: 30, Oct: 31, Nov: 30, Dec: 21 === 144 days * 86400 === 12441600
-    except(test_galactic.secsBetween(epicodusStart, epicodusGraduation).toEqual(12441600))
+    // July: 1 day, Aug: 31 days, Sept: 30, Oct: 31, Nov: 30, Dec: 21 === 143 days * 86400 === 12355200 + 3600 (for daylight savings in November) = 12358800
+    expect(test_galactic.secsBetween(epicodusStart, epicodusGraduation)).toEqual(12358800)
   });
 });
