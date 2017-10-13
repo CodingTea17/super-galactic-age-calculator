b653485dd18bf78819a3a334444a670d8060e670
Dawson Mortenson <dawson.mortenson@gmail.com>
2017-10-13 12:40:12 -0700

    added life expectancy data and a way to calculate an estimate based on country, gender, and gender

--- spec/galactic-spec.js
+++ spec/galactic-spec.js
@@ -6,30 +6,40 @@ describe('Galactic', () => {
     test_galactic = new Galactic();
   });
 
-  it('accepts the an age and returns the number of milliseconds', () => {
+  it('accepts an age and returns the number of milliseconds', () => {
     expect(test_galactic.milliAge(21)).toEqual(662695447680)
   });
 
-  it('accepts to dates to compare the time (in seconds) between them', () => {
+  it('accepts two dates to compare the time (in seconds) between them', () => {
     let epicodusStart = {month:7, day:31, year:2017};
     let epicodusGraduation = {month:12, day:21, year:2017};
     // July: 1 day, Aug: 31 days, Sept: 30, Oct: 31, Nov: 30, Dec: 21 === 143 days * 86400 === 12355200 + 3600 (for daylight savings in November) = 12358800
     expect(test_galactic.secsBetween(epicodusStart, epicodusGraduation)).toEqual(12358800)
   });
 
-  it('returns the age of a human in Mercury years', () => {
+  it('accepts an age and returns the age of a human in Mercury years', () => {
     expect(test_galactic.mercuryYears(21)).toEqual(5.04);
   });
 
-  it('returns the age of a human in Venus years', () => {
+  it('accepts an age and returns the age of a human in Venus years', () => {
     expect(test_galactic.venusYears(21)).toEqual(13.02);
   });
 
-  it('returns the age of a human in Mars years', () => {
+  it('accepts an age and returns the age of a human in Mars years', () => {
     expect(test_galactic.marsYears(21)).toEqual(39.48);
   });
 
-  it('returns the age of a human in Jupiter years', () => {
+  it('accepts an age and returns the age of a human in Jupiter years', () => {
     expect(test_galactic.jupiterYears(21)).toEqual(249.06);
+  });
+
+  it('returns the estimated male life expectancy using the provided information', () => {
+    expect(test_galactic.life(1995, 'male', 177)).toEqual("72.8");
+  });
+  it('returns the estimated female life expectancy using the provided information', () => {
+    expect(test_galactic.life(1990, 'female', 177)).toEqual("78.7");
+  });
+  it('returns the estimated female life expectancy when born in 2015 using the provided information', () => {
+    expect(test_galactic.life(2015, 'female', 177)).toEqual("81.6");
   });
 });
