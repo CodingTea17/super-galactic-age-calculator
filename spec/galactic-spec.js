4e3ccbe585ad364e4ea6c0869ed917575aaf0be7
Dawson Mortenson <dawson.mortenson@gmail.com>
2017-10-13 09:14:23 -0700

    add test for a function called secsBetween. It takes two dates and returns the number of seconds between those to custom date objects

--- spec/galactic-spec.js
+++ spec/galactic-spec.js
@@ -6,7 +6,14 @@ describe('Galactic', () => {
     test_galactic = new Galactic();
   });
 
-  it('takes the an age and returns the number of milliseconds', () => {
+  it('accepts the an age and returns the number of milliseconds', () => {
     expect(test_galactic.milliAge(21)).toEqual(662695447680)
+  });
+
+  it('accepts to dates to compare the time (in seconds) between them', () => {
+    let epicodusStart = {month:7, day:31, year:2017};
+    let epicodusGraduation = {month:12, day:21, year:2017};
+    // July: 1 day, Aug: 31 days, Sept: 30, Oct: 31, Nov: 30, Dec: 21 === 144 days * 86400 === 12441600
+    except(test_galactic.secsBetween(epicodusStart, epicodusGraduation).toEqual(12441600))
   });
 });
